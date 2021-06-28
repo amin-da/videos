e18e99ec2240580a2e6d9c5f87f5dd461cf0b4e3
amin-da <darabniya@yahoo.com>
2021-06-28 10:57:47 +0330

    vidoes web app with JS

---
+++ app.js
@@ -0,0 +1,36 @@
+const arrows = document.querySelectorAll(".movie-list-arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((item, index) => {
+  //ایجاد محدودیت به تناسب آیتم های موجود
+  const itemLength = movieLists[index].querySelectorAll("img").length;
+  let clickCounter = 0;
+  //کدها برای عمل کلیک و جابه جایی آیتم ها
+  item.addEventListener("click", () => {
+    // محدودیت کلیک متناسب با سایز صفحه
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemLength - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[index].style.transform = `translateX(${movieLists[index]
+        .computedStyleMap()
+        .get("transform")[0].x.value + 300}px)`;
+    } else {
+      movieLists[index].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+});
+
+// Theme Logic
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.navbar-container,.sidebar,.right-menu-icon,.toggle,.movie-list-title,.a"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach(item => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
